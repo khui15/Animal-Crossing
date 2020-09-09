0dd8b25c4746565f60dad447b34add1ffa108e31
Eunike Utojo <e.utojo@student.unsw.edu.au>
2020-09-09 17:39:37 +1000

    Renamed site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import "../styles/styles.scss"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Eunike Utojo" />
     <div className="page-container">
       <h1>Hello it's me.</h1>
       <p>Hello and welcome to my playground! Only a handful of people 
